ea6b082f3055410ea0855a412e2e4249ebeb3cea
MN <bmdaetl@gmail.com>
2025-09-15 14:06:17 +0200

    Intégration des templates

--- src/components/todoList/template.js
+++ src/components/todoList/template.js
@@ -1,11 +1,41 @@
-import './styles.css';
-
+import "./styles.css";
+﻿
 export default function (todoList) {
-    // on peut faire une fonction anonyme, on donne un nom dans le fichier TodoList.js
     return `
-        <h1>Ma TodoList</h1>
-        <ul class="todolist">
-            ${ todoList.todos.map((todo) => todo.render()).join('') }
+    <section class="todoapp">
+        <header class="header">
+        <h1>todos</h1>
+        <input
+            class="new-todo"
+            placeholder="What needs to be done?"
+            autofocus
+        />
+        </header>
+        <main class="main">
+        <div class="toggle-all-container">
+            <input class="toggle-all" type="checkbox" />
+            <label class="toggle-all-label" for="toggle-all"
+            >Mark all as complete</label
+            >
+        </div>
+        <ul class="todo-list">
+            ${todoList.todos.map((todo) => todo.render()).join("")}
         </ul>
+        </main>
+        <footer class="footer">
+        <span class="todo-count">1 item(s) left</span>
+        <ul class="filters">
+            <li><a href="#/" class="selected">All</a></li>
+            <li><a href="#/active">Active</a></li>
+            <li><a href="#/completed">Completed</a></li>
+        </ul>
+        <button class="clear-completed">Clear completed</button>
+        </footer>
+    </section>
+    <footer class="info">
+        <p>Double-click to edit a todo</p>
+        <p>Created by the TodoMVC Team</p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
     `;
 }
